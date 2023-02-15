f781074e147dcaae90c7285f692f72a0931d213f
srividya-sundaravadivelu <srivi18.2002@gmail.com>
2023-02-15 13:14:56 -0500

    week 3 changes to project

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake,CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
+
+
 
 # CarModelAdmin class
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+admin.site.register(CarMake,CarMakeAdmin)
 
 # Register models here
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,6 +10,13 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name + " " + self.description
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +26,23 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    name = models.CharField(max_length=30)
+    dealer_id = models.IntegerField()
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    year = models.DateField()
+    car_make = models.ForeignKey(CarMake,null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name + " " + self.type + " " + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
