449ab3b9c4b5c3b86e3428152122f2899e4c32ef
srividya-sundaravadivelu <srivi18.2002@gmail.com>
2023-02-17 13:09:49 -0500

    week 3 added proxies and NLU

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,10 +19,11 @@ urlpatterns = [
     path(route='login', view=views.login_request, name='login'), 
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-    
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-    path(route='', view=views.get_dealerships, name='index'),
+    
+    path('dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
