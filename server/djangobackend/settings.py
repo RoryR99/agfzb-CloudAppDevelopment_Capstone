53dc924f6a1d5450b97a4a1116f15f9b52aaf75e
srividya-sundaravadivelu <srivi18.2002@gmail.com>
2023-03-01 12:54:45 -0500

    deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
-
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'https://srivi182002-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai','https://srivi182002-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*']
+CSRF_TRUSTED_ORIGINS = [ "https://srivi182002-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/djangoapp/", "https://srivi182002-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 # Application definition
 
